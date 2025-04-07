71ef89849880d10a17d0248296c864f50233f0da
medidipeddisai <saimedidi999@gmail.com>
2025-04-08 00:35:10 +0530

    7 april updated

--- lib/utils/bbapi.dart
+++ lib/utils/bbapi.dart
@@ -10,4 +10,5 @@ class Bbapi {
   static const String getcategory="$baseUrl/bbaddpropcat";
   static const String addplantoproperty="$baseUrl/bbaddpropertyplan";
   static const String updateeuser = '$baseUrl/bbupdateuser';
+  static const String updateeusdd = '$baseUrl/bbupdateuser';
 }
